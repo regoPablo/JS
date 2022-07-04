170e2de9f36dad4e977f4093864d61016cf5bda4
regoPablo <pablo.rego26@gmail.com>
2022-07-04 08:25:13 +0200

    Segundo commit

---
+++ 2_JS_fundamentals_p2/challenge.js
@@ -0,0 +1,37 @@
+let bills = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+let tips = [];
+let totals = [];
+
+const calcTip = bills => {
+
+    let tip;
+
+    for(var i = 0; i < bills.length; i++){
+        if(bills[i] > 50 && bills[i] < 300){
+            tip = bills[i] * 0.15
+        } else {
+            tip = bills[i] * 0.20
+        }
+
+        tips.push(tip);
+        totals.push(tip + bills[i]);
+    }
+}
+
+const calcAverage = arr => {
+
+    let arrSum = 0;
+
+    for(var i = 0; i < arr.length; i++){
+        arrSum += arr[i];
+    }
+
+    return arrSum / arr.length;
+}
+
+calcTip(bills);
+calcAverage(bills)
+
+console.log(tips)
+console.log(totals)
---
+++ 2_JS_fundamentals_p2/ejercicio_objetos.js
@@ -0,0 +1,30 @@
+const Mark = {
+    fullName: 'Mark',
+    mass: 78,
+    height: 169,
+    calcBMI: function(){
+        this.bmi = this.mass / this.height * this.height
+        return this.bmi;
+    }
+}
+
+const John = {
+    fullName: 'John',
+    mass: 92,
+    height: 195,
+    calcBMI: function(){
+        this.bmi = this.mass / this.height * this.height
+        return this.bmi;
+    }
+}
+
+Mark.calcBMI();
+John.calcBMI();
+
+if(Mark.bmi > John.bmi){
+    console.log(`Mark BMI (${Mark.bmi}) is higher then John BMI (${John.bmi})`)
+} else if(John.bmi > Mark.bmi){
+    console.log(`John BMI (${John.bmi}) is higher then Mark BMI (${Mark.bmi})`)
+} else {
+    console.log(`Both have the same BMI (${johnBMI})`)
+}
